caabf318e4ec7f40b83bbea9ae0f5f74d740a556
Umpoy <iumpoy@yahoo.com>
2018-10-10 16:30:01 -0700

    boiler plate for index

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const configDB = require('./config/database.js');
 mongoose.connect(configDB.url);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: 'string',
     saveUninitialized: true,
     resave: true
 }));
+
+app.set('view engine', 'ejs');
 
 // app.use('/', (req, res) => {
 //     res.send('Hello World');
