177f7340b658a6ba972bbf84fa661797f8d22e6a
Umpoy <iumpoy@yahoo.com>
2018-10-10 15:26:17 -0700

    added cookie-parser to app

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+    secret: 'string',
+    saveUninitialized: true,
+    resave: true
+}));
 
 app.use('/', (req, res) => {
     res.send('Hello World');
+    console.log('req.cookies: ', req.cookies);
+    console.log('=================');
+    console.log('req.session: ', req.session);
 });
 
 app.listen(port, () => {
