b06dbc0331ce552140f15f9fca6a297ffff4f07d
Umpoy <iumpoy@yahoo.com>
2018-10-11 00:37:32 -0700

    [bug fix}: wrote code in wrong spot

--- config/passport.js
+++ config/passport.js
@@ -54,20 +54,13 @@ module.exports = passport => {
                 User.findOne({ 'local.username': email }, (err, user) => {
                     if (err)
                         return done(err);
-                    if (user) {
-                        return done(null, false, req.flash('signupMessage', 'That email already taken'));
-                    } else {
-                        var newUser = new User();
-                        newUser.local.username = email;
-                        newUser.local.password = newUser.generateHash(password);
-
-                        newUser.save(function (err) {
-                            if (err) {
-                                throw err;
-                            }
-                            return done(null, newUser);
-                        })
+                    if (!user)
+                        return done(null, false, req.flash('loginMessage', 'No User found'));
+                    if (!user.validPassword(password)) {
+                        return done(null, false, req.flash('loginMessage', 'invalid password'));
                     }
+                    return done(null, user);
+
                 })
             })
         }
